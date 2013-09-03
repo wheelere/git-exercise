829c2026c6c0dffaab5c21c9161a11de4adaf8c6
wheelere <wheelere@grinnell.edu>
2013-09-03 10:43:15 -0500

    Update HelloGit.java

--- src/edu/grinnell/csc2067/wheelere/hello/HelloGit.java
+++ src/edu/grinnell/csc2067/wheelere/hello/HelloGit.java
@@ -5,6 +5,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello, GitHub!");
+		System.out.println("Well, I see why GitHub's pretty cool I guess.");
 	} // main(String[])
 	
 }
