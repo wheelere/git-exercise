a901a5212416c78e78e762e5510f9bb04720afd8
EarnestWheeler <wheelere@grinnell.edu>
2013-09-03 10:41:18 -0500

    Hello github

---
+++ src/edu/grinnell/csc2067/wheelere/hello/HelloGit.java
@@ -0,0 +1,10 @@
+package edu.grinnell.csc2067.wheelere.hello;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, GitHub!");
+	} // main(String[])
+	
+}
